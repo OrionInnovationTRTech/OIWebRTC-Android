0f590370127326aeb52e8cb912cb3ee5870d4469
Emin DENİZ <emndeniz@gmail.com>
2022-07-25 15:46:06 +0300

    OIWebRTC Dependency added

--- app/src/main/java/com/orioninnovation/tr/oiwebrtc/MainActivity.java
+++ app/src/main/java/com/orioninnovation/tr/oiwebrtc/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.orioninnovation.tr.oiwebrtc.rtc.PCObserver;
+
 public class MainActivity extends AppCompatActivity {
 
+    //TODO: We should have a sample app in here.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Dummy Observer to see Library compile is successful
+        PCObserver pcObserver = new PCObserver();
     }
 }
